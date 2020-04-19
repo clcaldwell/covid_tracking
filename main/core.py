389c236c31241d6bb94eb77594b1e98f32a41d3b
Coby Caldwell <cobycaldwell@gmail.com>
2020-04-19 16:53:32 -0700

    establish main module and connect to Heroku

--- main/core.py
+++ main/core.py
@@ -2,16 +2,6 @@
 from . import helpers
 import requests
 
-def get_hmm():
-    """Get a thought."""
-    return 'hmmm...'
-
-
-def hmm():
-    """Contemplation..."""
-    if helpers.get_answer():
-        print(get_hmm())
-
 api_response = requests.get('https://covidtracking.com/api/states?state=NY')
 print("COVID-19 Testing Results in", api_response.json()['state'])
 print("Positive:", api_response.json()['positive'])
