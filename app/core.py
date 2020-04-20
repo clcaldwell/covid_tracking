89f0deb60458d51907a3242b5aea1e600f73a9fa
Coby Caldwell <cobycaldwell@gmail.com>
2020-04-19 18:21:10 -0700

    import statistics

--- app/core.py
+++ app/core.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-from . import helpers
 import requests
 
-api_response = requests.get('https://covidtracking.com/api/states?state=NY')
-print("COVID-19 Testing Results in", api_response.json()['state'])
-print("Positive:", api_response.json()['positive'])
-print("Negative:", api_response.json()['negative'])
+def getCurrentStatsByState():
+    api_response = requests.get('https://covidtracking.com/api/states?state=NY')
+    #print("COVID-19 Testing Results in", api_response.json()['state'])
+    print(api_response.json()['positive'])
+    #print("Positive:", api_response.json()['positive'])
+    #print("Negative:", api_response.json()['negative'])
--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
-from flask import Flask 
+from flask import Flask
+import requests
   
 app = Flask(__name__) 
   
+import requests
+
+def getCurrentStatsByState():
+        api_response = requests.get('https://covidtracking.com/api/states?state=NY')
+        #print("COVID-19 Testing Results in", api_response.json()['state'])
+        return (api_response.json()['positive'])
+        #print("Positive:", api_response.json()['positive'])
+        #print("Negative:", api_response.json()['negative'])
+
 @app.route("/") 
 def home_view(): 
         return "<h1>Test Deploy to Heroku</h1>"
+        return "<h3>New York:" + (getCurrentStatsByState()) + "<h3>"
