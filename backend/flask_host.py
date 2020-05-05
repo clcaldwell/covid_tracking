4dd7c49dabf826847ba5adcbdd126f3ccbf23f12
Coby Caldwell <cobycaldwell@gmail.com>
2020-05-04 23:44:35 -0700

    merge working project

---
+++ backend/flask_host.py
@@ -0,0 +1,66 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow_jsonapi.flask import Relationship, Schema
+from flask_rest_jsonapi import ResourceRelationship, Api, ResourceDetail, ResourceList
+from marshmallow_jsonapi import fields
+from sqlalchemy import UniqueConstraint
+
+database_file = 'covid.sqlite'
+# Create a new Flask application
+app = Flask(__name__)
+
+# Set up SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + database_file
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class State(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    state = db.Column(db.String)
+    date = db.Column(db.Date)
+    positiveTotal = db.Column(db.Integer)
+    negativeTotal = db.Column(db.Integer)
+    deathTotal = db.Column(db.Integer)
+    __table_args__ = (
+        UniqueConstraint(state, date),
+    )
+
+# Create the table
+db.create_all()
+
+class StateSchema(Schema):
+    class Meta:
+        type_ = 'state'
+        self_view = "state_one"
+        self_view_kwargs = {'id': '<id>'}
+        self_view_many = 'state_many'
+
+    id = fields.Integer()
+    state = fields.Str(required=True)
+    date = fields.Date(required=True)
+    positiveTotal = fields.Integer()
+    negativeTotal = fields.Integer()
+    deathTotal = fields.Integer()
+
+class StateMany(ResourceList):
+    schema = StateSchema
+    data_layer = {'session': db.session,
+                  'model': State}
+
+class StateOne(ResourceDetail):
+    schema = StateSchema
+    data_layer = {'session': db.session,
+                  'model': State}
+
+api = Api(app)
+api.route(StateMany, 'state_many', '/states')
+api.route(StateOne, 'state_one', '/states/<int:id>')
+
+# main loop to run app in debug mode
+if __name__ == "__main__":
+    from waitress import serve
+    serve(
+        app,
+        host="127.0.0.1",
+        port=9999,
+        ipv6=False)
